169cd95d65b821e3be4fa8d9456728e13d6bcdab
Rosario Scavo <rosboxe98@gmail.com>
2020-11-05 12:21:50 +0100

    srt_formatter function
    srt_formatter function has been defined and comments for all the functions have been added

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import math
 import os
 import sys
 import time
-
 import pandas as pd
-import numpy as np
 
 
 # return the intervals' info after reading a json file from the filename path
@@ -21,6 +19,7 @@ def read_intervals(filename):
         return data['metadata'].values()
 
 
+# it creates a dataframe with all the annotations' information for each interval present in intervals
 def intervals_dataframe_creation(intervals):
     num_caption = 1
     df = pd.DataFrame(columns=['num_caption', 'caption', 'timestamp_start', 'timestamp_end'])
@@ -35,25 +34,40 @@ def intervals_dataframe_creation(intervals):
 
         df = df.append(new_row, ignore_index=True)
         num_caption += 1
+
     return df
 
 
-# it converts the time expressed in ss.ms (45.234) to hh:mm:ss,ms (00:00:45,234)
+# it converts the time (timestamp) expressed in ss.ms (45.234) to hh:mm:ss,ms (00:00:45,234)
 def timestamp_converter(timestamp):
     decimal, integer = math.modf(timestamp)
-    decimal*=1000
-    decimal=int(decimal)
+    decimal *= 1000
+    decimal = int(decimal)
+
     return time.strftime('%H:%M:%S', time.gmtime(integer)) + ",%i" % decimal
+
+
+# it formats the annotation information into the srt format
+def srt_formatter(num_caption, timestamp_start, timestamp_end, caption):
+    stamp = "%i\n" \
+            "%s --> %s\n" \
+            "%s\n\n" % (num_caption, timestamp_start, timestamp_end, caption)
+
+    return stamp
 
 
 # def srt_creation(file_name, dataframe):
 #     # f = open(file_name+".srt", "w")
 #     # f.write()
 #     # f.close()
-#     annotation = dataframe.loc[0]
-#     print("%i \n"
-#           "%5.3f" % (annotation['num_caption'], annotation['timestamp_start']))
-#     print()
+#     df = dataframe[:2]
+#     annotation = df.loc[1]
+#
+#     timestamp_start = timestamp_converter(annotation['timestamp_start'])
+#     timestamp_end = timestamp_converter(annotation['timestamp_end'])
+#
+#     stamp = srt_formatter(annotation['num_caption'], timestamp_start, timestamp_end, annotation['caption'])
+#     print(stamp)
 
 
 # main
@@ -75,6 +89,4 @@ if __name__ == '__main__':
 
     intervals = read_intervals(filename)
     df = intervals_dataframe_creation(intervals)
-    annotation = df.loc[0]
-    timestamp_converter(annotation['timestamp_start'])
     # srt_creation("video(4)", df)
