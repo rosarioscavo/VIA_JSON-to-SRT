53206c4773ffe6e5ebf7dad1f40bfdf5377c2aa8
Rosario Scavo <rosboxe98@gmail.com>
2020-11-05 11:54:48 +0100

    timestamp converter function
    The function timestamp_converter has been defined

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
 import argparse
 import json
+import math
 import os
 import sys
+import time
 
 import pandas as pd
 import numpy as np
-
-
-def intervals_dataframe_creation(intervals):
-    num_caption = 1
-    df = pd.DataFrame(columns=['num_caption', 'caption', 'timecodes_start', 'timecodes_end'])
-
-    for interval_info in intervals:
-        caption = interval_info['av']['1']
-        timecodes_start = interval_info['z'][0]
-        timecodes_end = interval_info['z'][1]
-
-        new_row = {'num_caption': num_caption, 'caption': caption, 'timecodes_start': timecodes_start,
-                   'timecodes_end': timecodes_end}
-
-        df = df.append(new_row, ignore_index=True)
-        num_caption += 1
-    return df
 
 
 # return the intervals' info after reading a json file from the filename path
@@ -36,6 +21,41 @@ def read_intervals(filename):
         return data['metadata'].values()
 
 
+def intervals_dataframe_creation(intervals):
+    num_caption = 1
+    df = pd.DataFrame(columns=['num_caption', 'caption', 'timestamp_start', 'timestamp_end'])
+
+    for interval_info in intervals:
+        caption = interval_info['av']['1']
+        timestamp_start = interval_info['z'][0]
+        timestamp_end = interval_info['z'][1]
+
+        new_row = {'num_caption': num_caption, 'caption': caption, 'timestamp_start': timestamp_start,
+                   'timestamp_end': timestamp_end}
+
+        df = df.append(new_row, ignore_index=True)
+        num_caption += 1
+    return df
+
+
+# it converts the time expressed in ss.ms (45.234) to hh:mm:ss,ms (00:00:45,234)
+def timestamp_converter(timestamp):
+    decimal, integer = math.modf(timestamp)
+    decimal*=1000
+    decimal=int(decimal)
+    return time.strftime('%H:%M:%S', time.gmtime(integer)) + ",%i" % decimal
+
+
+# def srt_creation(file_name, dataframe):
+#     # f = open(file_name+".srt", "w")
+#     # f.write()
+#     # f.close()
+#     annotation = dataframe.loc[0]
+#     print("%i \n"
+#           "%5.3f" % (annotation['num_caption'], annotation['timestamp_start']))
+#     print()
+
+
 # main
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -46,10 +66,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     filename = "/home/rosarioscavo/Documents/dataset/Acquisizioni Lab ENIGMA Scavo/HoloLens/QC/temp3.json"
+    output = "/home/rosarioscavo/Documents/dataset/Acquisizioni Lab ENIGMA Scavo/HoloLens/QC"
 
     if args.file:
         filename = args.file
+    if args.output:
+        output = args.output
 
     intervals = read_intervals(filename)
     df = intervals_dataframe_creation(intervals)
-    print(df)
+    annotation = df.loc[0]
+    timestamp_converter(annotation['timestamp_start'])
+    # srt_creation("video(4)", df)
